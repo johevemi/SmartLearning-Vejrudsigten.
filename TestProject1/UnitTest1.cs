388d6e8576ba1d489ca4cba3927c8f6f9ea2b170
Jonas Michelsen <jvm@energinet.dk>
2021-10-24 14:38:00 +0200

    .

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -9,11 +9,11 @@ namespace TestProject1
     public class UnitTest1
     {
         [Theory]
-        [InlineData("Regn", 10, "Sne", -2, "Vejret skifter fra Sne til Regn med en stigning i temperatur fra -2 grader til 10 grader")]
-        [InlineData("Sne", -2, "Skyet", 5, "Vejret skifter fra Skyet til Sne med en fald i temperatur fra 5 grader til -2 grader")]
+        [InlineData("Regn", 39.99, "Sne", -2, "Vejret skifter fra Sne til Regn med en stigning i temperatur fra -2 grader til 39,99 grader")]
+        [InlineData("Sne", -19.99, "Skyet", 5, "Vejret skifter fra Skyet til Sne med en fald i temperatur fra 5 grader til -19,99 grader")]
         [InlineData("Klart vejr", 20, "Klart vejr", 20, "Det bliver igen Klart vejr med samme temperatur som i går på 20 grader")]
         [InlineData("Klart vejr", 40, "Klart vejr", 20, "Det er alt for varmt")]
-        [InlineData("Sne", -20, "Klart vejr", 20, "Det er alt for koldt")]
+        [InlineData("Sne", -20, "Sne", -2, "Det er alt for koldt")]
         public void Test1(string todayInfoConditions, double todayInfoTemperature, string yesterdayInfoConditions, double yesterdayInfoTemperature, string expected)
         {
             var sut = new WeatherMessageCreator();
